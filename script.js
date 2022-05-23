a011bc325a70585499cc61457f1ff6393bbc8582
Dwiki Nusanjaya <nusanjayadwiki@gmail.com>
2022-05-23 15:43:39 +0800

    versi 2.0

--- script.js
+++ script.js
@@ -18,55 +18,180 @@ const numbers = "0123456789";
 const symbols = "!@#$%^&*_-+=";
 
 btnGenerate.addEventListener("click", function getChar() {
-  let passChar = [];
-  let i = 0;
-  if (upperCase.checked) {
-    passChar += uCase;
-    i++;
-  }
-  if (lowerCase.checked) {
-    passChar += lCase;
-    i++;
-  }
-  if (number.checked) {
-    passChar += numbers;
-    i++;
-  }
-  if (symbol.checked) {
-    passChar += symbols;
-    i++;
-  }
-  if (length.value >= 100) {
-    alert(" password length at least under 100 characters");
-  }
-  if (i === 0) {
-    alert(" select at least one 1 option");
-  }
+   let passChar = [];
+   let i = 0;
+   if (upperCase.checked) {
+      passChar += uCase;
+      i++;
+   }
+   if (lowerCase.checked) {
+      passChar += lCase;
+      i++;
+   }
+   if (number.checked) {
+      passChar += numbers;
+      i++;
+   }
+   if (symbol.checked) {
+      passChar += symbols;
+      i++;
+   }
+   if (length.value >= 100) {
+      alert(" password length at least under 100 characters");
+   }
+   if (i === 0) {
+      alert(" select at least one 1 option");
+   }
 
-  // get random pass
-  let value = "";
+   // get random pass
+   let value = "";
 
-  for (let index = 0; index < length.value; index++) {
-    let pass = Math.floor(Math.random() * passChar.length);
-    value += passChar.charAt(pass);
-  }
+   for (let index = 0; index < length.value; index++) {
+      let pass = Math.floor(Math.random() * passChar.length);
+      value += passChar.charAt(pass);
+   }
 
-  // check
-  if (length.value >= 100) {
-    password.value = "";
-  } else {
-    password.value = value;
-  }
+   // check
+   if (length.value >= 100) {
+      password.value = "";
+   } else {
+      password.value = value;
+   }
 });
 
 // copy to clipboard
 const icon = document.getElementById("icon");
 copy.addEventListener("click", () => {
-  if (password.value == "") {
-    alert("create a password first, before copy to clipboard");
-  } else {
-    password.select();
-    document.execCommand("copy");
-    alert("text copied to clipboard");
-  }
+   if (password.value == "") {
+      alert("create a password first, before copy to clipboard");
+   } else {
+      password.select();
+      document.execCommand("copy");
+      alert("text copied to clipboard");
+   }
 });
+
+// particle js
+particlesJS.load("particles-js", "assets/particles.json", function () {
+   console.log("callback - particles.js config loaded");
+});
+particlesJS(
+   "particles-js",
+
+   {
+      particles: {
+         number: {
+            value: 80,
+            density: {
+               enable: true,
+               value_area: 800,
+            },
+         },
+         color: {
+            value: "#ffffff",
+         },
+         shape: {
+            type: "circle",
+            stroke: {
+               width: 0,
+               color: "#000000",
+            },
+            polygon: {
+               nb_sides: 5,
+            },
+            image: {
+               src: "img/github.svg",
+               width: 100,
+               height: 100,
+            },
+         },
+         opacity: {
+            value: 0.5,
+            random: false,
+            anim: {
+               enable: false,
+               speed: 1,
+               opacity_min: 0.1,
+               sync: false,
+            },
+         },
+         size: {
+            value: 5,
+            random: true,
+            anim: {
+               enable: false,
+               speed: 40,
+               size_min: 0.1,
+               sync: false,
+            },
+         },
+         line_linked: {
+            enable: true,
+            distance: 150,
+            color: "#ffffff",
+            opacity: 0.4,
+            width: 1,
+         },
+         move: {
+            enable: true,
+            speed: 6,
+            direction: "none",
+            random: false,
+            straight: false,
+            out_mode: "out",
+            attract: {
+               enable: false,
+               rotateX: 600,
+               rotateY: 1200,
+            },
+         },
+      },
+      interactivity: {
+         detect_on: "canvas",
+         events: {
+            onhover: {
+               enable: true,
+               mode: "repulse",
+            },
+            onclick: {
+               enable: true,
+               mode: "push",
+            },
+            resize: true,
+         },
+         modes: {
+            grab: {
+               distance: 400,
+               line_linked: {
+                  opacity: 1,
+               },
+            },
+            bubble: {
+               distance: 400,
+               size: 40,
+               duration: 2,
+               opacity: 8,
+               speed: 3,
+            },
+            repulse: {
+               distance: 200,
+            },
+            push: {
+               particles_nb: 4,
+            },
+            remove: {
+               particles_nb: 2,
+            },
+         },
+      },
+      retina_detect: true,
+      config_demo: {
+         hide_card: false,
+         background_color: "#b61924",
+         background_image: "",
+         background_position: "50% 50%",
+         background_repeat: "no-repeat",
+         background_size: "cover",
+      },
+   }
+);
